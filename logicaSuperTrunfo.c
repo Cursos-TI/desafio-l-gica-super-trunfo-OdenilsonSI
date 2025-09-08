dbaf1934a819f2f90e4a81d412b26bb9eb715ea1
OdenilsonSI <202508944391@alunos.estacio.br>
2025-09-08 11:07:10 -0300

    Update logicaSuperTrunfo.c
    criando cartas com atributos e comparando a  densidade populacional

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -7,37 +7,50 @@
 
 int main() {
     // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
+    // Atributo para Carta 1
+    char ct1_estado[] = "MA";
+    char ct1_codigo[] = "MA123";
+    char ct1_cidade[] = "São Luís";
+    int ct1_populacao = 2000;
+    float ct1_area = 100.5;
+    float ct1_pib = 5000.0;
+    int ct1_numero_pontos_turismo = 10;
 
+    // calculo para densidade populacional
+    float ct1_densidade_populacional = ct1_populacao / ct1_area;
+   
+    // calculo para pib per capita
+    float ct1_pib_per_capita = ct1_pib / ct1_populacao;
+
+    // Atributo para Carta 2
+    char ct2_estado[] = "RJ";
+    char ct2_codigo[] = "RJ456";
+    char ct2_cidade[] = "Rio de Janeiro";
+    int ct2_populacao = 2000;
+    float ct2_area = 200.5;
+    float ct2_pib = 10000.0;
+    int ct2_numero_pontos_turismo = 20;
+
+    // calculo para densidade populacional
+    float ct2_densidade_populacional = ct2_populacao / ct2_area;
     
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
+    // calculo para pib per capita
+    float ct2_pib_per_capita = ct2_pib / ct2_populacao;
 
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
+    //saidas
+    printf("----- O atributo escolhido para comparacao eh a densidade populacional --------\n");
+    printf(" -> A densidade populacional da carta 1 eh %f\n", ct1_densidade_populacional);
+    printf(" -> O pib per capita da carta 1 eh %f\n", ct1_pib_per_capita);
+    printf(" -> A densidade populacional da carta 2 eh %f\n", ct2_densidade_populacional);
+    printf(" -> O pib per capita da carta 2 eh %f\n", ct2_pib_per_capita);
 
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
-
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
-
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
-
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
-
-    return 0;
+    // Comparação de Atributos usando estrutura condicional composta
+    if (ct1_densidade_populacional < ct2_densidade_populacional)
+    {
+        printf(" -> A carta 1 venceu na densidade populacional\n");
+    }
+    else
+    {
+        printf(" -> A carta 2 venceu na densidade populacional\n");
+    }
 }
